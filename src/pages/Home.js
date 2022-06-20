f1113136d7e60c3545eab886f33674d4789fc228
Ajay Bhatia <Ajaybhatiawork2020@gmail.com>
2022-06-20 15:27:59 -0700

    fixed duplications with white space

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
 
-    if (!this.state.todos.find(t => t.content === todo.content)) {
+    if (!this.state.todos.find(t => t.content === todo.content.trim())) {
       todo.id = Math.random();
       // Create a array that contains the current array and the new todo item
       let new_list = [...this.state.todos, todo];
